2ebba13042dfecbc575197bbe3b5f34d6264f628
Muro Bakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2022-03-14 21:56:32 +0400

    added students json file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-// fetch('https://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then((response) => {
+    let students_data = [];
+    let table = document.querySelector('#students_data')
+    // console.log(response);
+    response.forEach(value => {
+      students_data.push('<tr>');
+      students_data.push('<td class="students_td">' + value.id + '</td>');
+      students_data.push('<td class="students_td">' + value.name + '</td>');
+      students_data.push('<td class="students_td">' + value.username + '</td>');
+      students_data.push('<td class="students_td">' + value.email + '</td>');
+      students_data.push('<td class="students_td">' + value.address + '</td>');
+    });
+    // table.innerHTML(students_data);
+    $('#students_table').append(students_data);
+  });
 
-
-$(document).ready(function () {
-  $.getJSON("")
-})
